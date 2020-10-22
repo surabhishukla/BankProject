977f213c8c6c7f1d7cb8af7e1a9e84566686c1ed
Surabhishukla <surabhi.shukla@83incs.com>
2020-10-22 20:58:33 +0530

    swagger on another project

---
+++ src/main/java/com/example/demo/SwaggerConfig.java
@@ -0,0 +1,34 @@
+//package com.example.demo;
+//
+//import org.springframework.context.annotation.Bean;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//
+//import java.util.Collections;
+//
+//public class SwaggerConfig {
+//
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
+//                .paths(PathSelectors.ant("/api/*"))
+//                .build()
+//                .apiInfo(apiInfo());
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfo(
+//                "My REST API",
+//                "Some custom description of API.",
+//                "API TOS",
+//                "Terms of service",
+//                new Contact("John Doe", "www.example.com", "myeaddress@company.com"),
+//                "License of API", "API license URL", Collections.emptyList());
+//    }
+//}
